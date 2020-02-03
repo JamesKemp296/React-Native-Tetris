b4db8e35e912aefdf631d691cab306a54ac2cd14
JamesKemp296 <jameskemp2796@yahoo.com>
2020-02-03 15:07:52 -0500

    added safeview for android and iOS

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  View
+} from 'react-native'
 
 import Tetris from './components/Tetris'
 
 const App = () => {
   return (
-    <View>
-      <Tetris />
-    </View>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View>
+        <Tetris />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default App
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  }
+})
